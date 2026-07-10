7546bd1818b3d07d165f4f97a2e562fb6ec68ea6
elmohawk <mrhawk2007@gmail.com>
2026-07-10 15:20:27 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,145 @@
-console.log("Welcome to KIVUStream 🚀");
+import { supabase } from "./supabase.js";
 
-const cards = document.querySelectorAll(".card");
+const trending =
+document.getElementById("trendingMovies");
 
-cards.forEach(card => {
-    card.addEventListener("click", () => {
-        alert("Movie page coming soon!");
-    });
+const recent =
+document.getElementById("recentMovies");
+
+const recommended =
+document.getElementById("recommendedMovies");
+
+async function loadTrending(){
+
+const { data,error } =
+await supabase
+
+.from("movies")
+
+.select("*")
+
+.eq("featured",true)
+
+.limit(12);
+
+if(error){
+
+console.log(error);
+
+return;
+
+}
+
+trending.innerHTML="";
+
+data.forEach(movie=>{
+
+trending.innerHTML+=movieCard(movie);
+
 });
+
+}
+
+async function loadRecent(){
+
+const { data } =
+await supabase
+
+.from("movies")
+
+.select("*")
+
+.order("created_at",{
+ascending:false
+})
+
+.limit(12);
+
+recent.innerHTML="";
+
+data.forEach(movie=>{
+
+recent.innerHTML+=movieCard(movie);
+
+});
+
+}
+
+async function loadRecommended(){
+
+const { data } =
+await supabase
+
+.from("movies")
+
+.select("*")
+
+.order("rating",{
+ascending:false
+})
+
+.limit(12);
+
+recommended.innerHTML="";
+
+data.forEach(movie=>{
+
+recommended.innerHTML+=movieCard(movie);
+
+});
+
+}
+
+function movieCard(movie){
+
+return `
+
+<div class="movie-card">
+
+<img
+src="${movie.poster}"
+loading="lazy">
+
+<div class="movie-overlay">
+
+<div class="movie-buttons">
+
+<button>
+<i class="fas fa-play"></i>
+</button>
+
+<button>
+<i class="fas fa-heart"></i>
+</button>
+
+</div>
+
+<div class="movie-details">
+
+<h3>${movie.title}</h3>
+
+<p>
+
+⭐ ${movie.rating}
+
+•
+
+${movie.year}
+
+</p>
+
+</div>
+
+</div>
+
+</div>
+
+`;
+
+}
+
+loadTrending();
+
+loadRecent();
+
+loadRecommended();
