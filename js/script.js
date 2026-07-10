b84ac643499575794a1d4f6f3663fa49bfd89509
elmohawk <mrhawk2007@gmail.com>
2026-07-10 15:03:36 +0200

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,9 @@
+console.log("Welcome to KIVUStream 🚀");
+
+const cards = document.querySelectorAll(".card");
+
+cards.forEach(card => {
+    card.addEventListener("click", () => {
+        alert("Movie page coming soon!");
+    });
+});
