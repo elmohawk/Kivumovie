984bee259caef236ff0e4b7abada6392a2b9aac5
elmohawk <mrhawk2007@gmail.com>
2026-07-10 15:07:42 +0200

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,13 @@
+import { createClient }
+from "https://esm.sh/@supabase/supabase-js";
+
+import {
+SUPABASE_URL,
+SUPABASE_KEY
+} from "./config.js";
+
+export const supabase =
+createClient(
+SUPABASE_URL,
+SUPABASE_KEY
+);
